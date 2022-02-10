5670d78a24376533ebd3e167e4781deae4a9dad3
Mimi Reyburn <mimireyburn@gmail.com>
2022-02-10 12:48:55 +0000

    Upload to GCS

---
+++ cloud.py
@@ -0,0 +1,23 @@
+'''
+
+'''
+# 
+from os import environ, path
+from google.cloud import storage
+
+
+#environ["GOOGLE_CREDENTIALS"] = "/home/pi/"
+
+PATH = path.dirname(__file__)
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    bucket_name = "food-inventory"
+    source_file_name = "/home/pi/2021-11-29-105425_1920x1080_scrot.png"
+    destination_blob_name = "blob"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
