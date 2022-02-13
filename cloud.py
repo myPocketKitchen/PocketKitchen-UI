55413a635c1f0af48b9bb12f1abd7e3c4b6f53b9
Mimi Reyburn <mimireyburn@gmail.com>
2022-02-13 17:12:31 +0000

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -6,18 +6,43 @@ from os import environ, path
 from google.cloud import storage
 
 
-#environ["GOOGLE_CREDENTIALS"] = "/home/pi/"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/pi/ServiceKey.json'
 
-PATH = path.dirname(__file__)
+storage_client = storage.Client()
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    bucket_name = "food-inventory"
-    source_file_name = "/home/pi/2021-11-29-105425_1920x1080_scrot.png"
-    destination_blob_name = "blob/"
+"""
+Create a New Bucket
+"""
 
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_filename(source_file_name)
+my bucket = storage_client.get_bucket('food-inventory')
+
+# Upload file
+
+def upload_to_bucket(blob_name, file_path, bucket_name): 
+    try: 
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(file_path)
+        return True
+    except Exception as e: 
+        print(e)
+        return False
+
+file_path = '/home/pi/2021-11-29-105425_1920x1080_scrot.png' 
+upload_to_bucket('Image 1', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png' ,'food_inventory'))
+upload_to_bucket('blob/Image 2', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png' ,'food_inventory'))
+
+
+
+# def upload_blob(bucket_name, source_file_name, destination_blob_name):
+#     """Uploads a file to the bucket."""
+#     bucket_name = "food-inventory"
+#     source_file_name = "/home/pi/2021-11-29-105425_1920x1080_scrot.png"
+#     destination_blob_name = ""
+
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(bucket_name)
+#     blob = bucket.blob(destination_blob_name)
+
+#     blob.upload_from_filename(source_file_name)
