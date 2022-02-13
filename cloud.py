e6fe64f142ffe04438c93ae81ee0027616c0c6d1
Mimi Reyburn <mimireyburn@gmail.com>
2022-02-13 17:25:19 +0000

    toggle create bucket

--- cloud.py
+++ cloud.py
@@ -13,25 +13,28 @@ storage_client = storage.Client()
 """
 Create a New Bucket
 """
-
+bucket_name = 'food-inventory'
+bucket = storage_client.bucket(bucket_name)
+bucket.location = 'US'
+bucket = storage_client.create_bucket(bucket)
 
 my_bucket = storage_client.get_bucket('food-inventory')
 
-# Upload file
+# # Upload file
 
-def upload_to_bucket(blob_name, file_path, bucket_name): 
-    try: 
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-        blob.upload_from_filename(file_path)
-        return True
-    except Exception as e: 
-        print(e)
-        return False
+# def upload_to_bucket(blob_name, file_path, bucket_name): 
+#     try: 
+#         bucket = storage_client.get_bucket(bucket_name)
+#         blob = bucket.blob(blob_name)
+#         blob.upload_from_filename(file_path)
+#         return True
+#     except Exception as e: 
+#         print(e)
+#         return False
 
-file_path = '/home/pi/2021-11-29-105425_1920x1080_scrot.png' 
-upload_to_bucket('Image 1', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png') ,'food_inventory')
-upload_to_bucket('blob/Image 2', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png') ,'food_inventory')
+# file_path = '/home/pi/2021-11-29-105425_1920x1080_scrot.png' 
+# upload_to_bucket('Image 1', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png') ,'food_inventory')
+# upload_to_bucket('blob/Image 2', os.path.join(file_path, '2021-11-29-105425_1920x1080_scrot.png') ,'food_inventory')
 
 
 
