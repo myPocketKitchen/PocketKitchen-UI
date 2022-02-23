87fa9b8dd904c37c98c66db5ff7eebc816ccae6d
Mimi Reyburn <mimireyburn@gmail.com>
2022-02-23 08:14:38 +0000

    Update photo.py

--- photo.py
+++ photo.py
@@ -11,7 +11,7 @@ camera = PiCamera()
 for i in range(5):
     sleep(3)
     timestamp = '{}'.format(datetime.datetime.now())
-    camera.capture('/home/pi/images/{}.jpg'.format(timestamp))
+    camera.capture('/home/pi/food-cam/images/{}.jpg'.format(timestamp))
 
 
 # camera.stop_preview()
