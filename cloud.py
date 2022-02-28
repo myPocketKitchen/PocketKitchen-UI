c025389d22a3e527cbdba071c71f59e4b0a2f12c
Mimi Reyburn <mimireyburn@gmail.com>
2022-02-28 12:02:25 +0000

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -40,11 +40,11 @@ def upload_to_bucket(blob_name, file_path, bucket_name):
 
 # file_path = '/home/pi' 
 
-file_path = '/home/pi/food_images/' 
+file_path = '/home/pi/food_images/*.jpg' 
 
 for filename in glob.glob(file_path):
     print(filename)
-    # upload_to_bucket('{}'.format(filename), os.path.join(file_path, '2022-02-23 07:50:54.551648.jpg') ,'food_inventory')
+    upload_to_bucket('{}'.format(filename), os.path.join('/home/pi/food_images/', '{}'.format(filename)) ,'food_inventory')
 
 
 # for filename in glob.glob(os.path.join(file_path, '/images/')): 
