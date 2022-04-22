75b12287fd5abb9365b7b007d363015b6100a229
Mimi Reyburn <mimireyburn@gmail.com>
2022-04-22 16:15:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.post('/getdata', (req, res) => {
 
 // Makes local port that enables rapid prototyping
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
