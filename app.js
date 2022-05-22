a07e774777ad5faa2606f549a443099089da85ee
Mimi Reyburn <mimireyburn@gmail.com>
2022-05-22 15:42:19 +0100

    commit get recipes working locally

--- app.js
+++ app.js
@@ -72,11 +72,35 @@ app.post('/expiry', function(req, res){
 });
 
 app.post('/getrecipes', (req, res) => {
-    // get recipes
-    // function delay(time) {
-    //     return new Promise(resolve => setTimeout(resolve, time));
-    // }
+    const PythonShell = require('python-shell').PythonShell;
 
+    function getIngredients() {
+        // getIngredients returns a promise that resolves to an ingredients array
+        return new Promise((resolve, reject) => {
+            var ingredients = [];
+            dbo.collection("records").find({}).project({"_id":0, "Item":1}).toArray(function(err, readout) {
+                readout.forEach(function (arrayElement) {
+                    ingredients.push(arrayElement.Item)
+                    })
+                    options = {
+                        args : ingredients.join(', ')
+                    }
+                    console.log(options)
+                    resolve(options);
+                })
+        });
+    }
+
+    function getRecipes(arg) {
+        return new Promise((resolve, reject) => {
+            PythonShell.run('words2vec_rec.py', arg, function (err) {
+                if (err) throw err;
+                console.log('finished');
+                resolve(); // resolve the empty promise
+            });
+        })
+    }
+ 
     function readJsonFileSync(filepath, encoding){
         if (typeof (encoding) == 'undefined'){
             encoding = 'utf8';
@@ -86,100 +110,151 @@ app.post('/getrecipes', (req, res) => {
     }
     
     function getConfig(file){
-    
         var filepath = __dirname + '/' + file;
         return readJsonFileSync(filepath);
     }
 
-    // var ingredients2 = [];
-    const PythonShell = require('python-shell').PythonShell;
-    
 
-    // let test = true;
-
-    function getIngredients() {
-        var ingredients = [];
-        dbo.collection("records").find({}).project({"_id":0, "Item":1}).toArray(function(err, readout) {
-            readout.forEach(function (arrayElement) {
-                ingredients.push(arrayElement.Item)
-                console.log("Hello from inside getIngredients")
-                console.log(ingredients)
-                // console.log(JSON.stringify(ingredients))
-                // console.log(ingredients)
-                // delay(1000).then(() => console.log('ran after 1 second1 passed'));
-                })
-            })
-            // return options = {
-            //     args: arg
-            // };
-        return ingredients;
-    };
-
-    // getIngredients();
-    // console.log(arg);
-    
-    
-
-
-    function getRecipes(arg) {
-        // console.log(arg)
-        PythonShell.run('words2vec_rec.py', arg, function (err) {
-            if (err) throw err;
-            console.log('finished');
+    const ingredients = getIngredients();
+    ingredients
+        .then(getRecipes)
+        .then(function () {
+            json = getConfig('sample.json');
+            console.log(json);
+            res.send(json);
         });
-    }
-
-    function sendRecipes() {
-        json = getConfig('sample.json');
-        res.send(json)
-        console.log("finito completo")
-    }
+});
 
 
 
-    async function waiter() {
-        const arg = await getIngredients();
-        console.log("hello from here 123123")
-        console.log(arg) 
-        getRecipes(arg);
-    }
+//     function getIngredients() {
+//         var ingredients = [];
+//         dbo.collection("records").find({}).project({"_id":0, "Item":1}).toArray(function(err, readout) {
+//             readout.forEach(function (arrayElement) {
+//                 ingredients.push(arrayElement.Item)
+//                 // console.log("Hello from inside getIngredients")
+//                 // console.log(ingredients)
+//                 // console.log(JSON.stringify(ingredients))
+//                 // console.log(ingredients)
+//                 // delay(1000).then(() => console.log('ran after 1 second1 passed'));
+//                 })
+//             })
+//             // return options = {
+//             //     args: arg
+//             // };
+//         return ingredients;
+//     };
+// });
 
-    async function waitress() {
-        const output = await waiter();
-        console.log(output)
-        sendRecipes();
-    }
+// app.post('/getrecipes', (req, res) => {
+//     // get recipes
+//     // function delay(time) {
+//     //     return new Promise(resolve => setTimeout(resolve, time));
+//     // }
+
+//     function readJsonFileSync(filepath, encoding){
+//         if (typeof (encoding) == 'undefined'){
+//             encoding = 'utf8';
+//         }
+//         var file = fs.readFileSync(filepath, encoding);
+//         return JSON.parse(file);
+//     }
+    
+//     function getConfig(file){
+    
+//         var filepath = __dirname + '/' + file;
+//         return readJsonFileSync(filepath);
+//     }
+
+//     // var ingredients2 = [];
+    // const PythonShell = require('python-shell').PythonShell;
     
 
-    waitress();
-    // sendRecipes();
+//     // let test = true;
 
-            // // console.log(options)
+//     function getIngredients() {
+//         var ingredients = [];
+//         dbo.collection("records").find({}).project({"_id":0, "Item":1}).toArray(function(err, readout) {
+//             readout.forEach(function (arrayElement) {
+//                 ingredients.push(arrayElement.Item)
+//                 console.log("Hello from inside getIngredients")
+//                 console.log(ingredients)
+//                 // console.log(JSON.stringify(ingredients))
+//                 // console.log(ingredients)
+//                 // delay(1000).then(() => console.log('ran after 1 second1 passed'));
+//                 })
+//             })
+//             // return options = {
+//             //     args: arg
+//             // };
+//         return ingredients;
+//     };
 
-            // PythonShell.run('words2vec_rec.py', options, function (err) {
-            //     if (err) throw err;
-            //     json = getConfig('sample.json');
-            //     res.send(json);
-            //     console.log('finished');
-            // });
+//     // getIngredients();
+//     // console.log(arg);
+    
+    
 
-            // async function asyncCall() {
-            //     console.log('calling');
-            //     await cabbage();
-            //     json = getConfig('sample.json');
-            //     res.send(json);
-            //     console.log("success!")
-            //     // console.logresult);
-            //     // expected output: "resolved"
-            //   }
+
+//     function getRecipes(arg) {
+//         // console.log(arg)
+//         PythonShell.run('words2vec_rec.py', arg, function (err) {
+//             if (err) throw err;
+//             console.log('finished');
+//         });
+//     }
+
+//     function sendRecipes() {
+//         json = getConfig('sample.json');
+//         res.send(json)
+//         console.log("finito completo")
+//     }
+
+
+
+//     async function waiter() {
+//         const arg = await getIngredients();
+//         console.log("hello from here 123123")
+//         console.log(arg) 
+//         getRecipes(arg);
+//     }
+
+//     async function waitress() {
+//         const output = await waiter();
+//         console.log(output)
+//         sendRecipes();
+//     }
+    
+
+//     waitress();
+//     // sendRecipes();
+
+//             // // console.log(options)
+
+//             // PythonShell.run('words2vec_rec.py', options, function (err) {
+//             //     if (err) throw err;
+//             //     json = getConfig('sample.json');
+//             //     res.send(json);
+//             //     console.log('finished');
+//             // });
+
+//             // async function asyncCall() {
+//             //     console.log('calling');
+//             //     await cabbage();
+//             //     json = getConfig('sample.json');
+//             //     res.send(json);
+//             //     console.log("success!")
+//             //     // console.logresult);
+//             //     // expected output: "resolved"
+//             //   }
             
-            // await asyncCall();
-            // // console.log(json)
-            // fix this!
+//             // await asyncCall();
+//             // // console.log(json)
+//             // fix this!
             
-});
+// });
 
 
 // Makes local port that enables rapid prototyping
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
