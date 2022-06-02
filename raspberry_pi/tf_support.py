bd1ada46474252ca80d2a113b55fdeb486f4cb16
Mimi Reyburn <mimireyburn@gmail.com>
2022-06-02 10:44:10 +0100

    Update tf_support.py

--- raspberry_pi/tf_support.py
+++ raspberry_pi/tf_support.py
@@ -4,7 +4,7 @@ from tflite_support.task import core
 from tflite_support.task import processor
 
 # Initialization
-base_options = core.BaseOptions(file_name=efnetone_edgetpu.tflite)
+base_options = core.BaseOptions(file_name=model_path.lite)
 detection_options = processor.DetectionOptions(max_results=2)
 options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
 detector = vision.ObjectDetector.create_from_options(options)
@@ -13,5 +13,5 @@ detector = vision.ObjectDetector.create_from_options(options)
 # detector = vision.ObjectDetector.create_from_file(model_path)
 
 # Run inference
-image = vision.TensorImage.create_from_file(example.jpg)
+image = vision.TensorImage.create_from_file(image_path)
 detection_result = detector.detect(image)
