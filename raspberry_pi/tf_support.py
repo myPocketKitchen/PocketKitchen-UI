84b32ecd9c8fa4399cfd4f67eea850110afa7f4c
Mimi Reyburn <mimireyburn@gmail.com>
2022-06-02 10:36:40 +0100

    Create tf_support.py

---
+++ raspberry_pi/tf_support.py
@@ -0,0 +1,17 @@
+# Imports
+from tflite_support.task import vision
+from tflite_support.task import core
+from tflite_support.task import processor
+
+# Initialization
+base_options = core.BaseOptions(file_name=model_path)
+detection_options = processor.DetectionOptions(max_results=2)
+options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
+detector = vision.ObjectDetector.create_from_options(options)
+
+# Alternatively, you can create an object detector in the following manner:
+# detector = vision.ObjectDetector.create_from_file(model_path)
+
+# Run inference
+image = vision.TensorImage.create_from_file(image_path)
+detection_result = detector.detect(image)
